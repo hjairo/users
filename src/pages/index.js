f6dd662935ee4d3f52d0f9bc6807e096f9622f0e
Jairo Hernandez <42648325+hjairo@users.noreply.github.com>
2021-07-24 13:23:40 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ function App() {
         </Center>
       </Box>
       <Stack bg={useColorModeValue('gray.100', 'gray.900')} spacing="16px" alignItems={'center'}>
-            <Accordion allowToggle bg="#fff" boxShadow="md" w="90%" m="16px">
+            <Accordion allowToggle bg="#fff" boxShadow="md" w="95%" m="16px">
               <AccordionItem>
                   <AccordionButton h="80px">
                     <UserIcon />
@@ -77,53 +77,6 @@ function App() {
                     </Stack>
                   </AccordionPanel>
               </AccordionItem>
-
-              {/* <AccordionItem>
-                  <AccordionButton h="80px">
-                    <Box flex="1" textAlign="left">
-                      <Text fontSize="12px" fontWeight="semibold" color="#4a4a4a">Alexander the Great</Text>
-                      <Text fontSize="10px" color="#4a4a4a">Administrator</Text>
-                      <Text fontSize="10px" color="#7e7e7e">email@email.com</Text>
-                    </Box>
-                    <AccordionIcon color="#7e7e7e" />
-                  </AccordionButton>
-                  <AccordionPanel pb={4}>
-                    <Stack flex="1" textAlign="left" pl="10px">
-                      <Box>
-                        <Text fontSize="10px" fontWeight="semibold" color="#4a4a4a">
-                          Address
-                        </Text>
-                        <Text fontSize="10px" color="#7e7e7e">
-                          1234 Not a Fake Street, Dallas, TX 75201
-                        </Text>
-                      </Box>
-                      <Box>
-                        <Text fontSize="10px" fontWeight="semibold" color="#4a4a4a">
-                          Phone
-                        </Text>
-                        <Text fontSize="10px" color="#7e7e7e">
-                          (555) 867-5309
-                        </Text>
-                      </Box>
-                      <Box>
-                        <Text fontSize="10px" fontWeight="semibold" color="#4a4a4a">
-                          Created At
-                        </Text>
-                        <Text fontSize="10px" color="#7e7e7e">
-                        1/1/70 12:01 AM
-                        </Text>
-                      </Box>
-                      <Box>
-                        <Text fontSize="10px" fontWeight="semibold" color="#4a4a4a">
-                          Last Logged In
-                        </Text>
-                        <Text fontSize="10px" color="#7e7e7e">
-                          1/2/70 12:02 AM
-                        </Text>
-                      </Box>
-                    </Stack>
-                  </AccordionPanel>
-              </AccordionItem> */}
             </Accordion>
       </Stack>
     </Container>
